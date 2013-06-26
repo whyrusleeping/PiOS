7a7bc6ef644a1401f6092925d12b1dda68a3317f
Jeromy <jeromyj@gmail.com>
2013-06-25 21:33:35 -0700

    draw the pretty picture on the screen, yeah\!

--- main.s
+++ main.s
@@ -7,51 +7,51 @@ b main
 main:
 mov sp,#0x8000
 
+mov r0,#1024
+mov r1,#768
+mov r2,#16
+bl InitializeFramBuffer
 
-pinNum .req r0
-pinFunc .req r1
-mov pinNum,#16
-mov pinFunc,#1
+teq r0,#0
+bne noError$
+
+mov r0,#16
+mov r1,#1
 bl SetGpioFunction
-.unreq pinFunc
-.unreq pinNum
-
-/* we only care about pin 16 right now */
-ptrn .req r4
-ldr ptrn,=pattern
-ldr ptrn,[ptrn]
-seq .req r5
-mov seq r5
-
-loop$:
-
-/* call SetGpio with parameters 16 (ok led) and 0 (low = on) */
-pinNum .req r0
-pinVal .req r1
-mov pinVal,#0
-mov pinNum,#16
+mov r0,#16
+mov r1,#0
 bl SetGpio
-.unreq pinNum
-.unreq pinVal
 
-mov r0,#0x80000
-bl Wait
+error$:
+b error$
 
-pinNum .req r0
-pinVal .req r1
-mov pinVal,#1
-mov pinNum,#16
-bl SetGpio
-.unreq pinNum
-.unreq pinVal
+noError$:
+fbInfoAddr .req r4
+mov fbInfoAddr,r0
 
-mov r0,#0x80000
-bl Wait
+render$:
+	fbAddr .req r3
+	ldr fbAddr,[fbInfoAddr,#32]
 
-b loop$
+	color .req r0
+	y .req r1
+	mov y,#768
+	drawRow$:
+		x .req r2
+		mov x,#1024
+		drawPixel$:
+			strh color,[fbAddr]
+			add fbAddr,#2
+			sub x,#1
+			teq x,#0
+			bne drawPixel$
+		
+		sub y,#1
+		add color,#1
+		teq y,#0
+		bne drawRow$
+	b render$
 
+.unreq fbAddr
+.unreq fbInfoAddr
 
-.section .data
-.align 2
-pattern:
-.int 0b11010110100101001011010001010110
